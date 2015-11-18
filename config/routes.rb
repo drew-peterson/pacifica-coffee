667e086ff0acea2d48fe13ddb7f99af7413b20ea
szow79 <szow79@gmail.com>
2015-11-18 15:19:14 -0800

    add stripe checkout test mode

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  resources :charges
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
