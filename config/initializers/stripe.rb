321c217cc3f9790390724a6780021ee0d528ecea
weeznog <drew.m.peterson@gmail.com>
2015-11-25 10:58:31 -0800

    Flex and media for nav bar

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,3 +4,5 @@ Rails.configuration.stripe = {
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+# PUBLISHABLE_KEY=pk_test_uksl51zPIOATT1torKitrzfN SECRET_KEY=sk_test_dLVNZ2c2HOfN6Ff5dcV1NbcF rails s
