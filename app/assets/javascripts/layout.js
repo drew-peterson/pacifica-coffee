95ce71ba0acf31cb40531bc8267452e5dacf4645
weeznog <drew.m.peterson@gmail.com>
2015-11-26 15:24:52 -0800

    Local storage for width intial setup

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,36 +1,61 @@
+var pacifica = {};
+pacifica.mobile = true;
+pacifica.test = "Drew Peterson"
+
+localStorage.setItem( 'pacifica', JSON.stringify( pacifica ))
+
+
+
+
 $(function(){
 
-  $(document).ready(function() {
-      // run test on initial page load
-      checkSize();
+  checkWidth()
+  $(window).resize(function(){
+    checkWidth()
+  })
 
-      // run test on resize of the window
-      $(window).resize(checkSize);
-  });
 
-  //Check if CSS exists then hide ul
-  function checkSize(){
-      if ($(".main-nav").css("flex-direction") == "column" ){
-          // your code here
-          $('#main-nav').hide()
 
-          if ($('#mobile-menu-btn').text()){
-            // prevent duplicate appends..
-          }else{
-            // add menu button
-            $('.nav-wrap').prepend("<button id='mobile-menu-btn' class='mobile-menu-btn'> menu </button>")
-          }
-          // show main-nav when not in mobile view
-      }else {
-        // show nav hide menu button
-          $('#main-nav').show()
-          $('#mobile-menu-btn').hide()
-      }
+
+}) // end of ready
+
+function checkWidth(){
+  if ($(window).width() < 769) {
+   console.log('Less than 769');
+
+   var data = getStorage()
+   data.mobile = true;
+   setStorage(data)
+
+
+   if(getStorage().mobile){
+    console.log(" True ")
+    }else{
+
+    }
+
+  // Tablet and above View
+  }else {
+   console.log('More than 960');
+   var data = getStorage()
+   data.mobile = false;
+   setStorage(data)
+
   }
+}
 
-  $('#main-header').on('click', '#mobile-menu-btn', function(e){
-    e.preventDefault();
-    $('#main-nav').toggle()
-  });
+function getStorage(){
+  return JSON.parse(localStorage.getItem('pacifica'));
+}
 
-})
+function setStorage(data){
+  return localStorage.setItem( 'pacifica', JSON.stringify( data ))
+
+}
+
+
+
+
+
+
+
