57665b8627a26c8f85b7408ab5469d0d702a307d
weeznog <drew.m.peterson@gmail.com>
2015-11-26 17:09:31 -0800

    hide show menu on mobile and desktop

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,57 +1,74 @@
-var pacifica = {};
-pacifica.mobile = true;
-pacifica.test = "Drew Peterson"
+// var pacifica = {};
+// pacifica.mobile = true;
+// pacifica.test = "Drew Peterson"
 
-localStorage.setItem( 'pacifica', JSON.stringify( pacifica ))
+// localStorage.setItem( 'pacifica', JSON.stringify( pacifica ))
 
 
 
+
+// $(function(){
+
+//   checkWidth()
+
+//   $(window).resize(function(){
+//     console.log("resize")
+//     checkWidth()
+//   })
+
+
+
+// }) // end of ready
+
+// function checkWidth(){
+//   if ($(window).width() < 769) {
+
+//    var data = getStorage()
+//    data.mobile = true;
+//    setStorage(data)
+
+//    // Mobile View
+//    if(getStorage().mobile){
+//       console.log('Less than 769');
+
+
+
+//     // Desktop
+//     }else{
+//       console.log('More than 960');
+
+//     }
+
+//   // Tablet and above View
+//   }else {
+//    var data = getStorage()
+//    data.mobile = false;
+//    setStorage(data)
+
+//   }
+// }
+
+// function getStorage(){
+//   return JSON.parse(localStorage.getItem('pacifica'));
+// }
+
+// function setStorage(data){
+//   return localStorage.setItem( 'pacifica', JSON.stringify( data ))
+
+// }
+
+
+// ************************
+// Alternative hide show menu
 
 $(function(){
 
-  checkWidth()
-  $(window).resize(function(){
-    checkWidth()
+  // hide show mobile menu
+  $(document).on('click', '#mobile-menu-btn', function(){
+    $('#main-nav').slideToggle()
+
   })
-
-
-
-
 }) // end of ready
-
-function checkWidth(){
-  if ($(window).width() < 769) {
-   console.log('Less than 769');
-
-   var data = getStorage()
-   data.mobile = true;
-   setStorage(data)
-
-
-   if(getStorage().mobile){
-    console.log(" True ")
-    }else{
-
-    }
-
-  // Tablet and above View
-  }else {
-   console.log('More than 960');
-   var data = getStorage()
-   data.mobile = false;
-   setStorage(data)
-
-  }
-}
-
-function getStorage(){
-  return JSON.parse(localStorage.getItem('pacifica'));
-}
-
-function setStorage(data){
-  return localStorage.setItem( 'pacifica', JSON.stringify( data ))
-
-}
 
 
 
