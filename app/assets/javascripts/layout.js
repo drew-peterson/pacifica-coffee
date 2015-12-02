cbc1651c7104e234ac2ed385e2003b8e0bce6363
szow79 <szow79@gmail.com>
2015-12-02 12:53:52 -0800

    login page mobile view

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -65,12 +65,12 @@ $(function(){
 
   // hide show mobile menu
   $(document).on('click', '#mobile-menu-btn', function(){
-    $('#main-nav').slideToggle()
+    $('#main-nav').slideToggle();
 
   })
 
   $(document).on('click', '#mobile-filter-btn', function(){
-    $('.filter').slideToggle()
+    $('.filter').slideToggle();
 
   })
 }) // end of ready
