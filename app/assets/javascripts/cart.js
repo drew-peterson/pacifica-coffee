63dd3ea09b80cc70c15239c5f038f9de8a2e8a21
weeznog <drew.m.peterson@gmail.com>
2015-12-09 08:41:47 -0800

    Add wraper for address + item

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -6,18 +6,21 @@ $(function(){
 
   stripeBtn.css('background', '#4285f4')
   stripeBtnSpan.css('background', 'none')
+
   stripeBtn.css('box-shadow', "none")
   stripeBtnSpan.css('box-shadow', "none")
+
   stripeBtnSpan.css('font-shadow', "none")
+
   stripeBtnSpan.css('font-size', "20px")
 
-
+// transition
   stripeBtn.hover(function(){
-    $(this).css('background', '#00c853').fadein()
+    $(this).css('background', '#00c853')
   },function(){
-    $(this).css('background', '#4285f4').fadeout()
+    $(this).css('background', '#4285f4')
   })
-
+  // change text
   stripeBtnSpan.text('Checkout')
 
 })
