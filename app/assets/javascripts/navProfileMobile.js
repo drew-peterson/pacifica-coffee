39bc1c6f0dc8a9863be33a6c1b7202eac8c5cece
weeznog <drew.m.peterson@gmail.com>
2015-12-11 18:26:51 -0800

    Add javascript gets for redirectings cards

--- app/assets/javascripts/navProfileMobile.js
+++ app/assets/javascripts/navProfileMobile.js
@@ -1,13 +1,25 @@
 "use strict";
 
+
+
 $(function(){
+  // Variables
+  var hambugerBtn = $('#mobile-menu-btn')
+  var navbar = $('#main-nav')
+  var profileModal = $('#navProfileModal')
+
+// functions
+  var closeModal = function(){
+    // hide modal
+    $(profileModal).removeClass('slide-left');
+    $(profileModal).fadeOut()
+
+    // fade in hidden navigations
+    $('.nav-container').fadeIn();
+  }
+
 
   $(document).on('click', '#profileNavBtn', function(e){
-
-    // Variables
-    var hambugerBtn = $('#mobile-menu-btn')
-    var navbar = $('#main-nav')
-    var profileModal = $('#navProfileModal')
 
     // hide nav menu and reset dropdown
     $(navbar).fadeOut();
@@ -17,25 +29,34 @@ $(function(){
     $(hambugerBtn).removeClass('is-active');
 
     // Show Modal
-
     $(profileModal).fadeIn()
     $(profileModal).addClass('slide-left');
 
     //bind click event to close
     $('#main-header').one('click', '#close-btn', function(){
 
-      // hide modal
-      $(profileModal).removeClass('slide-left');
-      $(profileModal).fadeOut()
-
-      // fade in hidden navigations
-      $('.nav-container').fadeIn();
+      closeModal();
 
       // show the navbar when tablet and above;
       if ($(window).width() > 769) {
         $(navbar).fadeIn();
       }
     })
+  })
+    // Order history click
+  $('#navProfileModal').on('click', '.history-col', function(e){
+    // redirect to order history
+    $.get('/users/order_history')
+    // close modal
+    closeModal()
+  })
+
+  //profile click
+  $('#navProfileModal').on('click','.profile-col',function(e){
+    //redirect to profile
+    $.get('#')
+    // close modal
+    closeModal()
   })
 
 }) // end of ready function
