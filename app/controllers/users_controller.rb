58be2688ae2731a350f0c84baf6c7f3a36970b68
szow79 <szow79@gmail.com>
2015-12-13 15:35:57 -0800

    restful routes for orders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def new
    @disable_nav = true;
   end
-
+  
+  # create new user
   def create
     user = User.new(user_params)
     if user.save
@@ -56,40 +57,6 @@ class UsersController < ApplicationController
 
   def cart
     @addresses = Address.where(user_id: current_user.id)
-  end
-
-  def order_history
-    @orders = Order.where(user_id: current_user.id)
-    @order_totals = []
-    @orders.each do |order|
-      cart_total(order.item_ids_quantities)
-      @order_totals << @cart_total
-    end
-  end
-
-  def single_order_history
-    @order = Order.find(params[:id])
-    find_items(@order.item_ids_quantities)
-  end
-
-  def last_order
-    order = Order.last
-    find_items(order.item_ids_quantities)
-
-    prices = []
-    @items.each do |item, qty|
-      prices << item.price * qty.to_i
-    end
-
-    @total = "$#{ prices.inject(:+) }.00"
-    @date = Date.parse("#{order.created_at}")
-
-    # multiple locals have to use {items: @items} in html just @items
-    @html = view_context.render 'partials/profile_history_last', locals: {items: @items, date: @date , total: @total}
-
-    respond_to do |format|
-      format.json  { render :json => {html: @html, date: @date, total: @total} } # don't do msg.to_json
-    end
   end
 
   def checkout
