1cbda141d3ea3ff2dd3a2c685d548ec60e49b68d
weeznog <drew.m.peterson@gmail.com>
2015-12-21 09:53:40 -0800

    Add javascript to hide show modal, basic style to modal

--- app/assets/javascripts/quickview.js
+++ app/assets/javascripts/quickview.js
@@ -6,7 +6,18 @@ $(function(){
     console.log('click')
     // select the modal for item
     var modal = $(this).next()
-    modal.css('background', 'blue')
+
+    // add styles to show the modal
+    modal.addClass('quickview-container-show')
+
+
+    // Close button - not using same btn so I toggle will not work...
+    $('.item-card').on('click', '.quickviewBtn-close', function(e){
+      e.preventDefault()
+      var modal = $(this).parent()
+      modal.removeClass('quickview-container-show')
+    })
+
 
   })
 
