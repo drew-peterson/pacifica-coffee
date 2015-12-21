e8b8e9762e4479a0ea867d6474eccb232146bc2b
weeznog <drew.m.peterson@gmail.com>
2015-12-21 13:35:14 -0800

    Fix quickview close

--- app/assets/javascripts/quickview.js
+++ app/assets/javascripts/quickview.js
@@ -3,7 +3,7 @@ $(function(){
 
   $('.item-card').on('click', '.quickviewBtn', function(e){
     e.preventDefault();
-    console.log('click')
+
     // select the modal for item
     var modal = $(this).next()
 
@@ -14,7 +14,8 @@ $(function(){
     // Close button - not using same btn so I toggle will not work...
     $('.item-card').on('click', '.quickviewBtn-close', function(e){
       e.preventDefault()
-      var modal = $(this).parent()
+
+      var modal = $(this).closest('.quickview-container')
       modal.removeClass('quickview-container-show')
     })
 
