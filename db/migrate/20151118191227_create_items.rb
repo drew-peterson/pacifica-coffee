23564f0d19ab0eb24c800473e59b42b35fa2018b
weeznog <drew.m.peterson@gmail.com>
2015-12-31 17:51:40 -0800

    Add small image url to seed

--- db/migrate/20151118191227_create_items.rb
+++ db/migrate/20151118191227_create_items.rb
@@ -1,9 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string :item_name, :sku, :large_image, :small_image
+      t.string :item_name, :sku, :large_image, :small_image, :caffeine, :roast, :region
       t.integer :quantity, :price
       t.text :item_description
+
 
       t.timestamps null: false
     end
--- db/migrate/20160101011138_add_caffeine_and_roast_and_region_to_item.rb
+++
@@ -1,7 +0,0 @@
-class AddCaffeineAndRoastAndRegionToItem < ActiveRecord::Migration
-  def change
-    add_column :items, :caffeine, :string
-    add_column :items, :roast, :string
-    add_column :items, :region, :string
-  end
-end
