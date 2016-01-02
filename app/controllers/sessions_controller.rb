9e9f21d912b52db71e694bd283c8b1b948d3badd
weeznog <drew.m.peterson@gmail.com>
2016-01-02 14:47:37 -0800

    Add message for no login found and pointer for submit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+
   def new
     @disable_nav = true
   end
@@ -9,13 +9,14 @@ class SessionsController < ApplicationController
     user = User.find_by(user_name: params[:user_name])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
+      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/items'
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login', notice: 'Incorrect Username or Password'
+      flash[:no_user_found] = 'Incorrect Username or Password'
+      redirect_to '/login'
     end
   end
 
