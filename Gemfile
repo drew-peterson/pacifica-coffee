3fff53bf285acd070d5f8965ee14927fd08d5530
weeznog <drew.m.peterson@gmail.com>
2016-01-02 15:37:47 -0800

    Add hirb and check to see if username is already created

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 #heroku push said to do so
 ruby '2.0.0'
 
+gem 'hirb'
 #autoprefixer -- drewp
 gem "autoprefixer-rails"
 #heroku asset pipeline -- drew
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,22 @@ class UsersController < ApplicationController
 
   # create new user
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    # check to see if username already exists
+
+    if User.find_by(user_name: user_params[:user_name]) == nil
+      user = User.new(user_params)
+
+      if user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      end
     else
+      # user already exists
+      flash[:user_taken] = "Sorry that User Name is already taken"
       redirect_to '/signup'
     end
+
+
   end
 
   def add_to_cart
