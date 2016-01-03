f431cbf30fbd3d272d2bc085d9f7721dd2d3ca56
weeznog <drew.m.peterson@gmail.com>
2016-01-03 13:25:58 -0800

    Password validation works, styles and transitions added

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,22 +1,25 @@
 $(function(){
 
  $('.confirm-psw').on('keyup',function(){
-
     var password = $('.password-psw').val()
     var confirm = $(this).val()
-    var submit = $('.signup-container input[type="submit]')
+    var submit = $('.signup-container input[type="submit"]')
 
     if(password === confirm){
+      console.log('correct')
       // enable btn
        $(submit).prop( "disabled", false );
       //Disable
-      $('.psw-label').removeClass('toolTip-slide');
+      $('.psw-label').removeClass('slide');
+      $( submit ).css('opacity', '1')
 
     }else {
+      console.log('wrong')
       // fade in label
-      $('.psw-label').addClass('toolTip-slide')
+      $('.psw-label').addClass('slide')
       // disable submit btn
       $( submit ).prop( "disabled", true ); //Disable
+      $( submit ).css('opacity', '0.1')
     }
   })
 
